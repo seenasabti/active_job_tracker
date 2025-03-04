315f7d6340fe7d1da09165501acff51b6a4a4cf4
Seena Sabti <seenasabti@gmail.com>
2025-03-04 10:32:32 -0500

    Add some tests

--- test/active_job_tracker_test.rb
+++ test/active_job_tracker_test.rb
@@ -1,7 +1,141 @@
 require "test_helper"
 
 class ActiveJobTrackerTest < ActiveSupport::TestCase
+  include ActiveJob::TestHelper
+
+  setup do
+    ActiveJobTrackerRecord.any_instance.stubs(:broadcast_replace_to).returns(true)
+  end
+
   test "it has a version number" do
     assert ActiveJobTracker::VERSION
   end
+
+  test "configuration defaults are set correctly" do
+    config = ActiveJobTracker.configuration
+
+    assert_equal 100, config.default_target
+    assert_equal 10, config.cache_threshold
+    assert_equal true, config.auto_broadcast
+    assert_equal "active_job_tracker/active_job_tracker", config.default_partial
+    assert_equal true, config.include_style
+  end
+
+  test "configuration can be modified" do
+    original_config = ActiveJobTracker.configuration.dup
+
+    ActiveJobTracker.configure do |config|
+      config.default_target = 200
+      config.cache_threshold = 5
+      config.auto_broadcast = false
+      config.default_partial = "custom/partial"
+      config.include_style = false
+    end
+
+    config = ActiveJobTracker.configuration
+    assert_equal 200, config.default_target
+    assert_equal 5, config.cache_threshold
+    assert_equal false, config.auto_broadcast
+    assert_equal "custom/partial", config.default_partial
+    assert_equal false, config.include_style
+
+    ActiveJobTracker.configure do |config|
+      config.default_target = original_config.default_target
+      config.cache_threshold = original_config.cache_threshold
+      config.auto_broadcast = original_config.auto_broadcast
+      config.default_partial = original_config.default_partial
+      config.include_style = original_config.include_style
+    end
+  end
+
+  test "trackable method raises error when arguments are empty" do
+    job = Job.new()
+    assert_raises(ArgumentError) do
+      job.trackable
+    end
+  end
+
+  test "trackable method returns first argument" do
+    tracked_object = HasTracker.create(name: "tracked_object")
+    job = Job.new(tracked_object, "other_arg")
+    assert_equal HasTracker, job.trackable.class
+  end
+
+  test "job lifecycle methods update tracker status correctly" do
+    tracked_object = HasTracker.create(name: "tracked_object")
+    job = Job.new(tracked_object)
+
+    job.initialize_tracker
+    assert_not_nil tracked_object.job
+
+    assert_equal "pending", tracked_object.job.status
+
+    job.mark_as_running
+    assert_equal "running", tracked_object.reload.job.status
+
+    job.active_job_tracker.current = job.active_job_tracker.target
+    job.mark_as_completed
+    assert_equal "completed", tracked_object.reload.job.status
+  end
+
+  test "active_job_tracker_target updates target" do
+    tracked_object = HasTracker.create(name: "tracked_object")
+
+    perform_enqueued_jobs do
+      Job.perform_later(tracked_object, 12)
+    end
+
+    assert_equal 12, tracked_object.job.target
+  end
+
+  test "active_job_tracker_progress updates current wihout cache" do
+    tracked_object = HasTracker.create(name: "tracked_object")
+
+    perform_enqueued_jobs do
+      Job.perform_later(tracked_object, 100, 12, false)
+    end
+
+    assert_equal 12, tracked_object.job.current
+  end
+
+  test "active_job_tracker_progress updates current with cache" do
+    tracked_object = HasTracker.create(name: "tracked_object")
+
+    perform_enqueued_jobs do
+      Job.perform_later(tracked_object, 100, 12, true)
+    end
+
+    assert_equal 12, tracked_object.job.current
+  end
+
+  test "active_job_tracker_log_error updates tracker with error info" do
+    tracked_object = HasTracker.create(name: "tracked_object")
+
+    perform_enqueued_jobs do
+      assert_raises(StandardError) do
+        Job.perform_later(tracked_object, 100, 12, true, true)
+      end
+    end
+
+    assert_equal "Error Message", tracked_object.job.error
+    assert_not_nil tracked_object.job.backtrace
+  end
+end
+
+class Job < ApplicationJob
+  include ActiveJobTracker
+
+  queue_as :default
+
+  def perform(csv_upload, target = 100, progress = 100, use_cache = true, throw_error = false)
+    active_job_tracker_target(target)
+    (1..progress).each do |_|
+      active_job_tracker_progress(cache: use_cache)
+      raise StandardError.new("Error Message") if throw_error
+    end
+  end
+end
+
+class HasTracker < ApplicationRecord
+  has_one :job, as: :active_job_trackable, class_name: "ActiveJobTrackerRecord"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ ENV["RAILS_ENV"] = "test"
 require_relative "../test/dummy/config/environment"
 ActiveRecord::Migrator.migrations_paths = [ File.expand_path("../test/dummy/db/migrate", __dir__) ]
 require "rails/test_help"
+require "mocha/minitest"
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
@@ -11,4 +12,26 @@ if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
   ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
   ActiveSupport::TestCase.fixtures :all
+end
+
+ActiveRecord::Base.connection.create_table :active_job_tracker_records, force: true do |t|
+  t.belongs_to :active_job_trackable, polymorphic: true, index: { name: "index_active_job_tracker_records_on_active_job_trackable", unique: true }
+
+  t.string :job_id, null: false, index: true
+  t.integer :status, default: 0, null: false
+  t.integer :current, default: 0, null: false
+  t.integer :target, default: 100, null: false
+
+  t.text :error
+  t.text :backtrace
+
+  t.datetime :started_at
+  t.datetime :completed_at
+
+  t.timestamps
+end
+
+ActiveRecord::Base.connection.create_table :has_trackers, force: true do |t|
+  t.string :name
+  t.timestamps
 end
